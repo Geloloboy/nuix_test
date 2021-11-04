51438ead7953d7e88f98e6c419ba49c0cae492d3
Geloloboy <addimanarig@gmail.com>
2021-11-04 22:19:37 +1100

    Initialize WDIO packages

---
+++ wdio.conf.ts
@@ -0,0 +1,37 @@
+export const config: WebdriverIO.Config = {
+    specs: [
+        './features/**/*.feature'
+    ],
+    exclude: [
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        maxInstances: 5,
+        browserName: 'chrome',
+        acceptInsecureCerts: true
+    }],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'http://localhost',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: ['chromedriver'],
+    framework: 'cucumber',
+    reporters: ['spec'],
+    cucumberOpts: {
+        require: ['./features/step-definitions/steps.ts'],
+        backtrace: false,
+        requireModule: [],
+        dryRun: false,
+        failFast: false,
+        format: ['pretty'],
+        snippets: true,
+        source: true,
+        profile: [],
+        strict: false,
+        tagExpression: '',
+        timeout: 60000,
+        ignoreUndefinedDefinitions: false
+    },
+}
